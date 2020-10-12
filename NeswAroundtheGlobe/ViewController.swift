b504cdeb86bde1242dd6a6e0475d7a233153b77c
Valeriia Zakharova <zakh.valeriia@gmail.com>
2020-10-12 16:18:46 +0300

    initial commit

---
+++ NeswAroundtheGlobe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NeswAroundtheGlobe
+//
+//  Created by Valeriia Zakharova on 12.10.2020.
+//  Copyright © 2020 Valeriia Zakharova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
